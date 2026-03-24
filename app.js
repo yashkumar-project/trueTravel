6aca78a5bc31e7e83d26c2c6259b8c5d70ac5950
Yash Kumar <yashkumar.connect@gmail.com>
2026-03-24 22:03:51 +0530

    adding dependencies

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// code is Cooming soon.
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+app.use(express.json());
+
+mongoUrl= 'mongodb://127.0.0.1:27017/truetravel';
+
+connectDB().then(()=>{
+    console.log("connect to Database");
+}).catch((err) =>{
+    console.log(err);
+});
+
+
+async function connectDB() {
+    await mongoose.connect(mongoUrl);
+}
+    
+        
+
+app.get('/', (req, res) => {
+    res.send("hello world");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
